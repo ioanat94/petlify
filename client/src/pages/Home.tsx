9b22638698758cd0f00825bc9ca918c6c63b5265
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-09-21 09:45:29 +0300

    Render all products on Home page (no styling)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <h1>HOME</h1>
-}
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from 'redux/hooks';
+import { fetchProductsThunk, Product } from 'redux/slices/productsSlice';
+import { RootState } from 'redux/store';
 
-export default Home
+const Home = () => {
+  const products = useAppSelector(
+    (state: RootState) => state.products.allProducts
+  );
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProductsThunk());
+  }, [dispatch]);
+
+  const handleRenderProducts = (products: Product[]) => {
+    return products.map((product) => (
+      <div>
+        <img src={product.img} alt='' width='200px' />
+        <h4>{product.name}</h4>
+        <p>{product.description}</p>
+        <div>
+          <span>{product.categories.pet}</span>
+          <span>{product.categories.subcategory}</span>
+        </div>
+        <p>{product.variants}</p>
+        <p>{product.sizes}</p>
+        <p>{product.price}</p>
+      </div>
+    ));
+  };
+
+  return <div>{handleRenderProducts(products)}</div>;
+};
+
+export default Home;
