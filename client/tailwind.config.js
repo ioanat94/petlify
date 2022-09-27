3b4951f3f69df3341135daff1133340faa8bc787
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-09-27 15:41:37 +0300

    Add Tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'mainFont': ['Montserrat', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        'mainBlue': '#5a61cb',
+        'mainYellow': '#f4cd57',
+        'mainGrey': '#8c8c8c',
+      },
+    },
   },
   plugins: [],
 };
