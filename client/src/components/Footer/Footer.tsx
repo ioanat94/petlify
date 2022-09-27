70ffff1a04b7d17459e380d1f725e58c9761a2b6
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-09-27 15:42:01 +0300

    Create Footer

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center h-16 bg-mainBlue text-white'>
+      Copyright © 2022 Petlify Oy
+    </div>
+  );
+};
+export default Footer;
