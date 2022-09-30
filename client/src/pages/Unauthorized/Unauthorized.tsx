82d1b13f2f5262ffc74e40dd6283951288f206c2
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-09-30 16:03:50 +0300

    Style Unauthorized page

--- client/src/pages/Unauthorized/Unauthorized.tsx
+++ client/src/pages/Unauthorized/Unauthorized.tsx
@@ -1,4 +1,20 @@
+import AdminNavbar from 'components/AdminNavbar/AdminNavbar';
+
 const Unauthorized = () => {
-  return <div>Unauthorized</div>;
+  return (
+    <>
+      <AdminNavbar />
+      <div className='flex flex-col items-center gap-4 min-h-[calc(100vh-64px)] pt-20 bg-adminBlue text-white'>
+        <img src={require('../../assets/403.png')} alt='' />
+        <div className='text-4xl text-red-500 font-bold'>OOPS!</div>
+        <div className='text-xl'>
+          It seems like you do not have permission to complete this action.
+        </div>
+        <div className='text-xl'>
+          Please contact a system admin if you need additional permissions.
+        </div>
+      </div>
+    </>
+  );
 };
 export default Unauthorized;
